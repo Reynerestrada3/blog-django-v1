e40669454a93d19b6577b43a2d13576e1c2c0446
Reynerestrada3 <reynerestradamonges03@gmail.com.ve>
2025-03-11 17:35:47 -0400

    publication detail done

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from .models import Publication
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class PublicationListView (ListView):
     model = Publication
     template_name = 'publications-list.html'
+
+class PublicationDetailView(DetailView):
+    model = Publication
+    template_name = 'publication-details.html'
